9e308252d06c57c78a1b2c9b614bfa40cfb8b0fe
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2022-03-30 15:31:08 +0200

    Projecto Spring boot adicionado

---
+++ controlo-transportes-cantinho-do-ceu/src/test/java/mz/co/cantinho/ceu/controlo/transportes/controlotransportescantinhodoceu/ControloTransportesCantinhoDoCeuApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.cantinho.ceu.controlo.transportes.controlotransportescantinhodoceu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControloTransportesCantinhoDoCeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
