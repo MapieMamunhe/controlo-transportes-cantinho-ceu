2186f3f01e9117cb31de84efde7cecbf2fef29d2
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2022-05-07 22:59:06 +0200

    Mensagem de confirmacao para cadastro de carrinha funcional Ficheiro alert.html que contem as mensagens de confirmacao foi adicionado O ficheiro carinha.html foi actualizado para contemplar o codigo da mensagem de confirmacao de cadastro de carinha Ficheiros relacionados a bootstrap 5 foram adicionados

--- controlo-transportes-cantinho-do-ceu/src/main/java/mz/co/cantinho/ceu/controlo/transportes/controller/CarrinhaController.java
+++ controlo-transportes-cantinho-do-ceu/src/main/java/mz/co/cantinho/ceu/controlo/transportes/controller/CarrinhaController.java
@@ -1,16 +1,17 @@
 package mz.co.cantinho.ceu.controlo.transportes.controller;
 
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mz.co.cantinho.ceu.controlo.transportes.domain.Carrinha;
 import mz.co.cantinho.ceu.controlo.transportes.service.CarrinhaService;
@@ -19,35 +20,36 @@ import mz.co.cantinho.ceu.controlo.transportes.validator.CarrinhaValidator;
 @Controller
 @RequestMapping("/carrinhas")
 public class CarrinhaController {
-    
 
 	@Autowired
 	private CarrinhaService service;
-	
 
 	@Autowired
 	private CarrinhaValidator validator;
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(validator);
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar() {
 		return "listar/carrinhas";
 	}
-	
+
 	@GetMapping("/nova")
 	public String novo(Carrinha carrinha) {
 		return "/cadastros/carrinha";
 	}
+
 	@PostMapping("/salvar")
-	public String salvar(@Valid Carrinha carrinha, BindingResult result) {
-		if(result.hasErrors()) {//verifica se campos têm  erros
-			return "/cadastros/carrinha";
+	public String salvar(@Valid Carrinha carrinha, BindingResult result, RedirectAttributes attr) {
+		if (!result.hasErrors()) {// verifica se campos têm erros
+			service.gravar(carrinha);
+			attr.addFlashAttribute("success", "Carinha gravada");
+		} else {
+			attr.addFlashAttribute("fail", "Erro ao cadastrar a Carinha");
 		}
-		service.gravar(carrinha);
 		return "redirect:/carrinhas/nova";
 	}
 
