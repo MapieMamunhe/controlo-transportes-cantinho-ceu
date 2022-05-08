8ca534bc1d8d40b2a943cc3dddc9a099db5b7892
Mapie Victorino Mamunhe <mmamunhe@gmail.com>
2022-05-08 09:45:49 +0200

    Mensagem de confirmacao para funcionario adicionado O metodo salvar do FuncionarioController foi actualizado para contemplar a mensagem de confirmacao

--- controlo-transportes-cantinho-do-ceu/src/main/java/mz/co/cantinho/ceu/controlo/transportes/controller/FuncionarioController.java
+++ controlo-transportes-cantinho-do-ceu/src/main/java/mz/co/cantinho/ceu/controlo/transportes/controller/FuncionarioController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mz.co.cantinho.ceu.controlo.transportes.domain.CidadeDistrito;
 import mz.co.cantinho.ceu.controlo.transportes.domain.ContaFuncionario;
@@ -34,101 +35,105 @@ public class FuncionarioController {
 
 	@Autowired
 	private FuncionarioService funcionarioService;
-	
+
 	@Autowired
 	private ContaFuncionarioService contaFuncionarioService;
-	
+
 	@Autowired
 	private MotoristaService motoristaService;
-	
+
 	@Autowired
 	private EducadoraService educadoraService;
-	
+
 	@Autowired
 	private PerfilService perfilService;
-	
+
 	@Autowired
 	private FuncionarioValidator funcionarioValidator;
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(funcionarioValidator);
 	}
-	
-    @GetMapping("/novo")
-    public String novo(Funcionario funcionario){
-    	return "/cadastros/funcionario";
-    }
+
+	@GetMapping("/novo")
+	public String novo(Funcionario funcionario) {
+		return "/cadastros/funcionario";
+	}
 
 	@GetMapping("/listar")
-    public String listar(ModelMap model){
-		//model.addAttribute("funcionario", )
-    	return "/listar/funcionarios";
-    }
-    
-    @PostMapping("/salvar")
-    public String salvar(@Valid Funcionario funcionario, BindingResult result){
-    	//Telefone alternativo é opcional. Ao clicar em submit é enviada uma string vazia, no
-    	//entanto a coluna tem a constraint 'UNIQUE', logo há necessidade de passar um nulo.
-    	if(funcionario.getTelefoneAlternativo().equals("")) {
-    		funcionario.setTelefoneAlternativo(null);
-    	}
-    			
-    	if(funcionario.getEmail().equals("")) {//Mesmo que telefone alternativo
-    		funcionario.setEmail(null);
-    	}		
-    	
-    	if(result.hasErrors()) {//verifica se campos têm erros
-    		return "/cadastros/funcionario";
-    	}
-    	
-    	funcionarioService.gravar(funcionario);
-    	criarConta(funcionario);
-    	registarPorPapel(funcionario);
-    	return "redirect:/funcionarios/novo";
-    }
-    
-    //Regista funcionário de acordo com o papel (mototrista ou educadora)
-    private void registarPorPapel(Funcionario funcionario) {
-    	switch(funcionario.getPapel()) {
-    		case "Motorista":
-    			Motorista motorista = new Motorista();
-    			motorista.setFuncionario(funcionario);
-    			motoristaService.gravar(motorista);
-    			break;
-    			
-    		case "Educadora":
-    			Educadora educadora = new Educadora();
-    			educadora.setFuncionario(funcionario);
-    			educadoraService.gravar(educadora);
-    			break;
-    			
-    		default:
-    			break;
-    	}
-    }
-    
-    //Cria conta de funcionário.
-    private void criarConta(Funcionario funcionario) {
-    	ContaFuncionario contaF = new ContaFuncionario();
-    	contaF.setFuncionario(funcionario);
-    	contaF.setPerfil(perfilService.buscarPorNome(funcionario.getPapel()));
-    	contaF.setPalavraPasse("0000");
-    	contaFuncionarioService.gravar(contaF);
-    }
-    
-    @ModelAttribute("funcionarios")
-    public List<ContaFuncionario> funcionarios(){
-    	return contaFuncionarioService.buscarTodos();
-    }
-    
-    @ModelAttribute("bairro")
-    public CidadeDistrito[] bairros() {
-    	return CidadeDistrito.values();
-    }
-    
-    @ModelAttribute("perfis")
-    public List<Perfil> perfis(){
-    	return perfilService.buscarTodos();
-    }
+	public String listar(ModelMap model) {
+		// model.addAttribute("funcionario", )
+		return "/listar/funcionarios";
+	}
+
+	@PostMapping("/salvar")
+	public String salvar(@Valid Funcionario funcionario, BindingResult result, ModelMap model,
+			RedirectAttributes attr) {
+		// Telefone alternativo é opcional. Ao clicar em submit é enviada uma string
+		// vazia, no
+		// entanto a coluna tem a constraint 'UNIQUE', logo há necessidade de passar um
+		// nulo.
+		if (funcionario.getTelefoneAlternativo().equals("")) {
+			funcionario.setTelefoneAlternativo(null);
+		}
+
+		if (funcionario.getEmail().equals("")) {// Mesmo que telefone alternativo
+			funcionario.setEmail(null);
+		}
+
+		if (result.hasErrors()) {// verifica se campos têm erros
+			model.addAttribute("fail", "Erro ao cadastrar Funcionário");
+			return "/cadastros/funcionario";
+		}
+		funcionarioService.gravar(funcionario);
+		criarConta(funcionario);
+		registarPorPapel(funcionario);
+		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso");
+		return "redirect:/funcionarios/novo";
+	}
+
+	// Regista funcionário de acordo com o papel (mototrista ou educadora)
+	private void registarPorPapel(Funcionario funcionario) {
+		switch (funcionario.getPapel()) {
+			case "Motorista":
+				Motorista motorista = new Motorista();
+				motorista.setFuncionario(funcionario);
+				motoristaService.gravar(motorista);
+				break;
+
+			case "Educadora":
+				Educadora educadora = new Educadora();
+				educadora.setFuncionario(funcionario);
+				educadoraService.gravar(educadora);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	// Cria conta de funcionário.
+	private void criarConta(Funcionario funcionario) {
+		ContaFuncionario contaF = new ContaFuncionario();
+		contaF.setFuncionario(funcionario);
+		contaF.setPerfil(perfilService.buscarPorNome(funcionario.getPapel()));
+		contaF.setPalavraPasse("0000");
+		contaFuncionarioService.gravar(contaF);
+	}
+
+	@ModelAttribute("funcionarios")
+	public List<ContaFuncionario> funcionarios() {
+		return contaFuncionarioService.buscarTodos();
+	}
+
+	@ModelAttribute("bairro")
+	public CidadeDistrito[] bairros() {
+		return CidadeDistrito.values();
+	}
+
+	@ModelAttribute("perfis")
+	public List<Perfil> perfis() {
+		return perfilService.buscarTodos();
+	}
 }
